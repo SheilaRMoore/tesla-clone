23812678600b8383c1fd4b83a19eed47f4f577fe
Sheila R Moore <sherexmykes@github.com>
2021-06-02 00:51:48 -0400

    made background and added buttons to homepage

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Home from './components/Home'
+import Header from './components/Header'
+import Section from './components/Section'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
